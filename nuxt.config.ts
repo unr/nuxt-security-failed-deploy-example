71e94a7000e5a19f218efe80901683485076933f
Paul Morrison <paul.unr@gmail.com>
2024-01-11 14:00:03 -0500

    removing nuxt security

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,32 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-
-	modules: ['nuxt-security'],
-	
-	security: {
-		headers: {
-			crossOriginEmbedderPolicy: process.env.NODE_ENV === "development" ? "unsafe-none" : "credentialless",
-			crossOriginResourcePolicy: "crossorigin",
-			crossOriginOpenerPolicy: "same-origin",
-			contentSecurityPolicy: {
-				"style-src": ["'self'", "'unsafe-inline'"],
-				"script-src-attr": ["'self'", "'unsafe-inline'"],
-				"script-src-elem": ["'self'", "'unsafe-inline'"],
-				"img-src": [
-					"'self'",
-					"'unsafe-inline'",
-					"data:",
-					"https://res.cloudinary.com",
-					"https://*.site.com",
-				],
-				"frame-ancestors": [
-					"'self'",
-					"https://*.site.com",
-					"http://localhost:3000"
-				]
-			}
-		}
-	},
-
 })
